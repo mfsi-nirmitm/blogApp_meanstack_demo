0daaadf52e5950470dcaa8b91831c0c0563b95ad
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-09-17 12:13:03 +0530

    adding post create component
    adding post create component

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+// core modules
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// components
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
