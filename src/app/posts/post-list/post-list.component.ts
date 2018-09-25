a35224ba6a86a71ef615beafaca0b02ee0cc9710
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-09-25 23:58:38 +0530

    adding post dynamically

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -5,9 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './post-list.component.html'
 })
 export class PostListComponent {
-  posts = [
-    {title: 'First Post', content: 'This is the first post\'s content'},
-    {title: 'Second Post', content: 'This is the second post\'s content'},
-    {title: 'Third Post', content: 'This is the third post\'s content'}
-  ];
+  // posts = [
+  //   {title: 'First Post', content: 'This is the first post\'s content'},
+  //   {title: 'Second Post', content: 'This is the second post\'s content'},
+  //   {title: 'Third Post', content: 'This is the third post\'s content'}
+  // ];
+  posts = [];
 }
