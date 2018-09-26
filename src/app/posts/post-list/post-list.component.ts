bb4c29f71357639b112c33f48be1ce5a44fd682c
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-09-26 10:39:59 +0530

    added interface for post

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -10,5 +12,5 @@ export class PostListComponent {
   //   {title: 'Second Post', content: 'This is the second post\'s content'},
   //   {title: 'Third Post', content: 'This is the third post\'s content'}
   // ];
-  posts = [];
+  @Input() posts: Post[] = [];
 }
