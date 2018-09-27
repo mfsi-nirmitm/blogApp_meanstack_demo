469e8a62e281c7352cdaeb9fd9c9a35617af1d98
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-09-27 10:23:01 +0530

    passing the copy of posts instead of reference

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
@@ -7,17 +8,26 @@ import { PostsService } from '../posts.service';
   selector: 'app-post-list',
   templateUrl: './post-list.component.html'
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   {title: 'First Post', content: 'This is the first post\'s content'},
   //   {title: 'Second Post', content: 'This is the second post\'s content'},
   //   {title: 'Third Post', content: 'This is the third post\'s content'}
   // ];
   posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
 
   ngOnInit() {
     this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
   }
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
 
@@ -6,13 +7,19 @@ import { Post } from './post.model';
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
-    return this.posts;
+    return [...this.posts]; // return the copy not the reference
+  }
+
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
   }
 
   addPost(title: string, content: string) {
     const post: Post = {title: title, content: content};
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]);
   }
 }
