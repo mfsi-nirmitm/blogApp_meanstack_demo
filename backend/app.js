7421205461f975461c7f3721a3c18d31d80a05aa
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-09-29 13:55:54 +0530

    added api to get posts data

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,35 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("first middleware");
+  res.setHeader('Access-Control-Allow-Origin',"*");
+  res.setHeader(
+    'Access-Contorl-Allow-Header',
+    'Origin, X-Requested-Width, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('hello from express!');
+app.use('/api/posts',(req, res, next) => {
+  const posts = [
+    {
+      id: 'fadf12421l',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: "ksajflaj132",
+      title: "Second server-side post",
+      content: "This is coming form the server!"
+    }
+  ];
+  return res.status(200).json({
+    message: 'Posts fetched succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
