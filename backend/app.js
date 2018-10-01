43b2d006d04a64958d07d2f16cd717b126b4e82e
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-10-01 13:29:26 +0530

    added mongoose and connected to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://nirmit:SqFj5tPE032ihJg3@cluster0-hanrm.mongodb.net/test?retryWrites=true")
+  .then(() => {
+    cosole.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -20,7 +31,10 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts',(req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully'
