8ff14ae3d5a1c493e4f4f83a554130d02dca1fea
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-10-21 10:41:45 +0530

    added page sizes to get api
    added page sizes to get api

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -65,8 +65,15 @@ router.put('/:id', multer({storage: storage}).single('image'), (req, res, next) 
 });
 
 router.get('',(req, res, next) => {
-
-  Post.find().then(documents => {
+  const pageSize = +req.query.pageSize;
+  const currentPage = req.query.page;
+  const postQuery = Post.find();
+  if ( pageSize && currentPage) {
+    postQuery
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+  postQuery.then(documents => {
     console.log(documents);
     res.status(200).json({
       message: 'Posts fetched succesfully!',
