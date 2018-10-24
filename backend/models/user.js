2bfb08e5b7dd8b87f2f8102dbcc6f194e77fabac
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-10-24 17:33:23 +0530

    added user model
    added user model

---
+++ backend/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mangoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exprots = mongoose.model("User", userSchema);
