27da6f2aba4739790ff8d3ba8ae352aa7cd32884
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-10-25 01:05:45 +0530

    connected the signup frontend to signup api to register use in database
    connected the signup frontend to signup api to register use in database

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require("mangoose");
+const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
@@ -8,4 +8,4 @@ const userSchema = mongoose.Schema({
 
 userSchema.plugin(uniqueValidator);
 
-module.exprots = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
 const bcrypt = require("bcrypt");
 
+const User = require("../models/user");
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).then(hash => {
-    const user = new user({
+//  bcrypt.hash(req.body.password, 10).then((hash) => {
+    let hash = bcrypt.hashSync(req.body.password, 10);
+    const user = new User({
       email: req.body.email,
-      password: req.body.password
+      password: hash
     });
     user.save()
       .then(result => {
+        console.log("here 2");
         res.status(201).json({
           message: 'User created!',
           result: result
@@ -21,7 +24,7 @@ router.post('/signup', (req, res, next) => {
           error: err
         });
       });
-  });
+//  });
 
 });
 
