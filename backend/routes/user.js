26d0a189dcc22260f214c154229bf26544f90d7c
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-10-31 00:47:55 +0530

    update the login api

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,33 +5,31 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const router = express.Router();
 
-router.post('/signup', (req, res, next) => {
-//  bcrypt.hash(req.body.password, 10).then((hash) => {
-    let salt = bcrypt.genSaltSync(10);
-    let hash = bcrypt.hashSync(req.body.password, salt);
-    const user = new User({
-      email: req.body.email,
-      password: hash
-    });
-    user.save()
-      .then(result => {
-        res.status(201).json({
-          message: 'User created!',
-          result: result
-        });
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: err
-        });
+router.post("/signup", (req, res, next) => {
+  //  bcrypt.hash(req.body.password, 10).then((hash) => {
+  let salt = bcrypt.genSaltSync(10);
+  let hash = bcrypt.hashSync(req.body.password, salt);
+  const user = new User({
+    email: req.body.email,
+    password: hash
+  });
+  user
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: "User created!",
+        result: result
       });
-//  });
-
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+  //  });
 });
 
-router.post('/login', (req, res, next) => {
-  console.log('in login api');
-  let fetchedUser;
+router.post("/login", (req, res, next) => {
   User.find({ email: req.body.email })
     .then(user => {
       if (!user) {
@@ -39,33 +37,27 @@ router.post('/login', (req, res, next) => {
           message: "Auth failed"
         });
       }
-      console.log(user);
-      let hash1 = bcrypt.compareSync(req.body.password , user.password);
-      console.log(hash1);
-
-    })
-      .then(result => {
-        console.log(result);
-        if (!result) {
-          return res.status(401).json({
-            message: 'Auth failed'
-          });
-        }
-        const token = jwt.sign(
-          {email: fetchedUser.email, userId: fetchedUser._id},
-          'secret_this_should_be_longer',
-          { expiresIn:'1h' }
-        );
-        res.status(200).json({
-          token: token
-        });
-      })
-      .catch(err => {
+      let fetchedUser = user[0];
+      let result = bcrypt.compareSync(req.body.password, fetchedUser.password);
+      if (!result) {
         return res.status(401).json({
-          message: 'Auth failed'
+          message: "Auth failed"
         });
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "secret_this_should_be_longer",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token
       });
-    //});
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed"
+      });
+    });
 });
 
 module.exports = router;
