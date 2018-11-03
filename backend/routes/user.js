c51d05090d3a66b4a4819182038c6ca9176f58af
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-11-04 04:59:21 +0530

    added controller at backend
    added controller at backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,65 +1,11 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
-const User = require("../models/user");
+const UserController = require("../controllers/user");
+
 const router = express.Router();
 
-router.post("/signup", (req, res, next) => {
-  //  bcrypt.hash(req.body.password, 10).then((hash) => {
-  let salt = bcrypt.genSaltSync(10);
-  let hash = bcrypt.hashSync(req.body.password, salt);
-  const user = new User({
-    email: req.body.email,
-    password: hash
-  });
-  user
-    .save()
-    .then(result => {
-      res.status(201).json({
-        message: "User created!",
-        result: result
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: 'Invalid authentication credentials!'
-      });
-    });
-  //  });
-});
+router.post("/signup", UserController.createUser);
 
-router.post("/login", (req, res, next) => {
-  User.find({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({
-          message: "Auth failed"
-        });
-      }
-      let fetchedUser = user[0];
-      let result = bcrypt.compareSync(req.body.password, fetchedUser.password);
-      if (!result) {
-        return res.status(401).json({
-          message: "Auth failed"
-        });
-      }
-      const token = jwt.sign(
-        { email: fetchedUser.email, userId: fetchedUser._id },
-        "secret_this_should_be_longer",
-        { expiresIn: "1h" }
-      );
-      res.status(200).json({
-        token: token,
-        expiresIn: 3600,
-        userId: fetchedUser._id
-      });
-    })
-    .catch(err => {
-      return res.status(401).json({
-        message: "Invalid authentication credentials!"
-      });
-    });
-});
+router.post("/login", UserController.userLogin);
 
 module.exports = router;
